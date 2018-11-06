40ef17611f51b711421021a923a9b69a3506c92a
ry-rise <ry-rise@example.com>
2018-11-07 02:46:32 +0900

    固定マップのところのコードを削除

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -31,35 +31,6 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private GameObject exitPrefab;
     private Transform mapHolder;
     #endregion
-    #region マップ作成(32*32)
-    //public void Awake()
-    //{
-    //    mapHolder = new GameObject("Map").transform;
-    //    for (int x = -1; x < map_width + 1; x += 1)
-    //    {
-    //        for (int y = -1; y < map_height + 1; y += 1)
-    //        {
-    //            //床
-    //            GameObject toInstantiate = floorPrefab[0]/*[Random.Range(0, floorPrefab.Length)]*/;
-    //            //出口
-    //            if (x == 5 && y == 5)
-    //            {
-    //                toInstantiate = exitPrefab;
-    //            }
-    //            //外壁
-    //            if (x == -1 || x == map_width || y == -1 || y == map_height) 
-    //            {
-    //                toInstantiate = wallPrefab[0]/*[Random.Range(0, wallPrefab.Length)]*/;
-    //            }
-    //            GameObject instance = Instantiate(toInstantiate,
-    //                                              new Vector2(x, y),
-    //                                              Quaternion.identity,
-    //                                              mapHolder) as GameObject;
-    //            instance.transform.localScale = new Vector2(3,3);
-    //        }
-    //    }
-    //}
-    #endregion
     #region マップ生成
     public int[,] mapStatus;
     private Player playerPos;
