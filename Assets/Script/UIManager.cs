1f61d8b13dde50b1bd92e9ab817225a24f4b6195
ry-rise-parallels <ry-rise@example.com>
2019-01-12 01:58:22 +0900

    UIManagerのSerializeFieldをTextからGameObjectに

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -2,12 +2,16 @@
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
-    [SerializeField] private Text LevelText;
-    [SerializeField] private Text FloorText;
-    [SerializeField] private Text HPText;
-    [SerializeField] private Text SatietyText;
+    [SerializeField] private GameObject LevelObject;
+    [SerializeField] private GameObject FloorObject;
+    [SerializeField] private GameObject HPObject;
+    [SerializeField] private GameObject SatietyObject;
     [SerializeField] private GameObject InventoryScreen;
     [SerializeField] private GameObject Log;
+    private Text LevelText;
+    private Text FloorText;
+    private Text HPText;
+    private Text SatietyText;
     private Player player;
     private GameManager gameManager;
 
@@ -15,13 +19,25 @@ public class UIManager : MonoBehaviour {
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-	}
+        LevelText = LevelObject.GetComponent<Text>();
+        FloorText = FloorObject.GetComponent<Text>();
+        HPText = HPObject.GetComponent<Text>();
+        SatietyText = SatietyObject.GetComponent<Text>();
+    }
 	void Update ()
     {
         LevelText.text = $"LV:{player.Level.ToString()}";
         FloorText.text = $"{gameManager.FloorNumber.ToString()}F";
         HPText.text = $"HP:{player.HP.ToString()}";
         SatietyText.text = $"空腹度:{player.Satiety.ToString()}";
+
+        if (gameManager.GamePause == true)
+        {
+            LevelObject.SetActive(false);
+            FloorObject.SetActive(false);
+            HPObject.SetActive(false);
+            SatietyObject.SetActive(false);
+        }    
         //Iキーを押すとインベントリが表示/非表示
         if (Input.GetKeyDown(KeyCode.I))
         {
