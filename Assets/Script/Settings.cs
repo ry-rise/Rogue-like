e4190b63826af4348d00ac1b924e0e30d0ad4c8a
ry-rise <ry-rise@example.com>
2019-02-07 02:27:52 +0900

    設定画面とミキサーの導入準備

---
+++ Assets/Script/Settings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+    private int volume;
+    private Slider slider;
+	private void Start ()
+    {
+        slider = GameObject.Find("").GetComponent<Slider>();
+	}
+	
+	private void Update ()
+    {
+		
+	}
+}
