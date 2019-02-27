e8005719f680e625113c78f4233eb7f4293240ff
ry-rise <ry-rise@example.com>
2019-02-28 05:02:52 +0900

    画像差し込み、リネーム

---
+++ Assets/Script/ItemRiceBall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public sealed class ItemRiceBall : ItemBase
+{
+
+    protected override void Awake ()
+    {
+        Name = "おにぎり";
+        base.Awake();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("itemRiceBall");
+            PickUP();
+        }
+    }
+
+    protected override void PickUP()
+    {
+        base.PickUP();
+    }
+
+    public override void Use()
+    {
+        int i = Random.Range(0, recoveryAmount.Length);
+        if (player.Satiety + recoveryAmount[i] > player.MaxSatiety)
+        {
+            player.Satiety = player.MaxSatiety;
+        }
+        else
+        {
+            player.Satiety += recoveryAmount[i];
+        }
+        player.inventoryList.Remove(gameObject);
+        Destroy(gameObject);
+        gameManager.TurnPlayer = false;
+    }
+}
