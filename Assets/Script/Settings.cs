e86a68f8267b33a79f2f3a0e37c0217eb0891ba4
ry-rise <ry-rise@example.com>
2019-02-28 11:50:25 +0900

    設定からGameDataを削除できるようにした

--- Assets/Script/Settings.cs
+++ Assets/Script/Settings.cs
@@ -1,16 +1,25 @@
 ﻿using System.IO;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    private readonly string FileName = "//SettinsData.json";
+    private readonly string SettingsFileName = "//SettingsData.json";
+    private readonly string GameFileName = "//SaveData.json";
     [SerializeField] private AudioMixer audioMixer;
     private void Start()
     {
-        if (File.Exists($"{Application.persistentDataPath}{FileName}") == true)
+        if (File.Exists($"{Application.persistentDataPath}{SettingsFileName}") == true)
         {
             SettingsLoad();
+        }
+        if (File.Exists($"{Application.persistentDataPath}{GameFileName}")==false)
+        {
+            if (GameObject.Find("DataDeleteButton"))
+            {
+                GameObject.Find("DataDeleteButton").GetComponent<Button>().interactable = false;
+            }
         }
     }
     public void SettingsSave()
@@ -28,17 +37,25 @@ public class Settings : MonoBehaviour
             seVolume = SEVolume
         };
         string json = JsonUtility.ToJson(settingsData);
-        string path = $"{Application.persistentDataPath}{FileName}";
+        string path = $"{Application.persistentDataPath}{SettingsFileName}";
         File.WriteAllText(path, json);
     }
     public void SettingsLoad()
     {
-        string path = $"{Application.persistentDataPath}{FileName}";
+        string path = $"{Application.persistentDataPath}{SettingsFileName}";
         string json = File.ReadAllText(path);
         SettingsData restoreSettingsData = JsonUtility.FromJson<SettingsData>(json);
         audioMixer.SetFloat("MasterVol", restoreSettingsData.masterVolume);
         audioMixer.SetFloat("BGMVol", restoreSettingsData.bgmVolume);
         audioMixer.SetFloat("SEVol", restoreSettingsData.seVolume);
+    }
+    public void GameDataDelete()
+    {
+        File.Delete($"{Application.persistentDataPath}{GameFileName}");
+        if (GameObject.Find("DataDeleteButton"))
+        {
+            GameObject.Find("DataDeleteButton").GetComponent<Button>().interactable = false;
+        }
     }
 }
 
