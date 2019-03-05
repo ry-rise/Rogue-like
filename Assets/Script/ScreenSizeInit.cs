5f12517b082120e56eaa1253fe135b3757c705be
ry-rise <ry-rise@example.com>
2019-03-06 02:11:20 +0900

    ビルド時の画面サイズ設定を変更

---
+++ Assets/Script/ScreenSizeInit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ScreenSizeInit : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        Screen.SetResolution(1024, 576, false, 60);
+    }
+
+}
