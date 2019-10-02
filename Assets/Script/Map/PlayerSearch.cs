ba230f140b4a7fd05599bccef0897b701fa8e924
ry-rise-Mac <rise.ry.25691381.sac@ezweb.ne.jp>
2019-10-02 12:59:20 +0900

    PlayerSearchを追加

---
+++ Assets/Script/Map/PlayerSearch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSearch : MonoBehaviour 
+{
+	public class NodeA
+	{
+
+	}
+	public int CalculateCost(Vector2 playetPos,Vector2 enemyPos)
+	{
+		int cost=Mathf.Abs((int)enemyPos.x - (int)playetPos.x) + 
+		         Mathf.Abs((int)enemyPos.y - (int)playetPos.y);
+		return cost;
+	}
+}
